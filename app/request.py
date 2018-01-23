322b49f28df235a5bab72751c3de9a302bf09e9d
MrazTevin <millatevin@gmail.com>
2018-01-23 11:01:38 +0300

    made some imports

--- app/request.py
+++ app/request.py
@@ -1,3 +1,10 @@
 from app import app
+import urllib.request,json
+from models import movie
+
+Movie = movie.Movie
 # getting api api_key
 api_key = app.config['MOVIE_API_KEY']
+
+# getting the movie based url
+base_url = app.config["MOVIE_API_BASE_URL"]
